90aa1fc8368ac9c5c2df270f12bee36080c25d47
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-09 20:44:08 +0200

    add LodingTime

--- lib/pages/logIn.dart
+++ lib/pages/logIn.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
+  // log i time 
+  Duration get LodingTime => const Duration(milliseconds: 2000);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return FlutterLogin(
+      onLogin: onLogin, 
+      onRecoverPassword:  onRecoverPassword ,
+       )
   }
 }
