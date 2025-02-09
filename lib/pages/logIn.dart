8ee1c143713597ce10def101f0997f2d91868888
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-09 20:50:16 +0200

    add method forget paswoord

--- lib/pages/logIn.dart
+++ lib/pages/logIn.dart
@@ -6,11 +6,23 @@ class LoginScreen extends StatelessWidget {
   // log i time 
   Duration get LodingTime => const Duration(milliseconds: 2000);
 
+  // log in 
+
+  Future<String?>FeachUther(LoginData data){
+    return Future.delayed(LodingTime).then((value) => null);
+  }
+
+  // forget paswoord
+
+  Future<String?> recouverPassord(String data){
+    return Future.delayed(LodingTime).then((value)=>null);
+  }
+
   @override
   Widget build(BuildContext context) {
     return FlutterLogin(
-      onLogin: onLogin, 
-      onRecoverPassword:  onRecoverPassword ,
-       )
+      onLogin: FeachUther, 
+      onRecoverPassword:recouverPassord,
+       );
   }
 }
