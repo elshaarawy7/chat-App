c02af5925f7ee9bbc4b7b534bbcd03ab9be43463
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-09 20:36:04 +0200

    add pachage font awesome

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/pages/logIn.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,9 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     home: Scaffold(
-      backgroundColor: Colors.amber,
-     ),
+     home: LoginScreen(),
     );
   }
 }
