8f2266deef3d52e745ccc6c7bcb628317a289c96
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-10 14:59:45 +0200

    add file goggle_service json

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class chatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
      home: LoginScreen(),
     );
   }
